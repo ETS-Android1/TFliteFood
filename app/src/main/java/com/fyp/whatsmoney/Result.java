f08b318008e1442ecb6635556a5fb0e831bfc6d5
jhonydev <star.jk901@gmail.com>
2021-10-11 03:46:32 +0500

    All implemented

--- app/src/main/java/com/fyp/whatsmoney/Result.java
+++ app/src/main/java/com/fyp/whatsmoney/Result.java
@@ -2,12 +2,12 @@ package com.fyp.whatsmoney;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
 
     String str;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +19,33 @@ public class Result extends AppCompatActivity {
 
         str = result.replaceAll("\\[", "").replaceAll("]", "");
         TextView resultTextView = findViewById(R.id.textView);
+        TextView Suggestion = findViewById(R.id.textView2);
         resultTextView.setText(str);
 
-        // TODO: IMPLEMENT CNN ON THE BASIS OF BMI
+        try {
+            // TODO: IMPLEMENT CNN ON THE BASIS OF BMI
+            str = str.split(":")[1];
+            str = str.replace(" ", "");
+            str = str.split(",")[0];
+            str = str.replace(")", "");
+            double calories = Double.parseDouble(str);
+            double factor;
+            if (Utils.bmi < 18.5) {
+                factor = 18.5 - Utils.bmi;
+                int sugItems = (int) ((int) (factor * 3 * 3500) / calories);
+                String sug = "Eat " + sugItems + " these food items within 8 days to maintain your BMI";
+                Suggestion.setText(sug);
+            } else if (Utils.bmi > 25) {
+                factor = Utils.bmi - 25;
+                int sugItems = (int) ((int) (factor * 3 * 3500) / calories);
+                String sug = "Eat healthy - calories equivalent to " + sugItems + " of these food items must be burned to maintain BMI";
+                Suggestion.setText(sug);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
